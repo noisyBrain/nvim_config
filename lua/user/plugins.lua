78a473c5489199ff4b0423ed810133abaddd3e6d
Tomas <tomas.arregui@toremsoftware.com>
2023-01-29 11:36:00 -0300

    ADD: new plugins and removed old

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -47,11 +47,8 @@ return packer.startup(function(use)
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
 
  -- themes
-  use 'ghifarit53/tokyonight-vim'
+  use 'folke/tokyonight.nvim'
   use 'navarasu/onedark.nvim'
-  use 'luisiacc/gruvbox-baby'
-  use { "ellisonleao/gruvbox.nvim" }
-  use 'shaunsingh/nord.nvim'
 
   -- Lua
   use { "folke/zen-mode.nvim" }
@@ -68,30 +65,23 @@ return packer.startup(function(use)
 
 
 
-    -- cmp plugins
-    use({ "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin
-    use({ "hrsh7th/cmp-buffer", commit = "62fc67a2b0205136bc3e312664624ba2ab4a9323" }) -- buffer completions
-    use({ "hrsh7th/cmp-path", commit = "466b6b8270f7ba89abd59f402c73f63c7331ff6e" }) -- path completions
-    use({ "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" }) -- snippet completions
-    use({ "hrsh7th/cmp-nvim-lsp", commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8" })
-    use({ "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" })
-    use({ "glepnir/lspsaga.nvim", branch = "main" })
+  -- cmp plugins
+  use({ "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" }) -- The completion plugin
+  use({ "hrsh7th/cmp-buffer", commit = "62fc67a2b0205136bc3e312664624ba2ab4a9323" }) -- buffer completions
+  use({ "hrsh7th/cmp-path", commit = "466b6b8270f7ba89abd59f402c73f63c7331ff6e" }) -- path completions
+  use({ "saadparwaiz1/cmp_luasnip", commit = "a9de941bcbda508d0a45d28ae366bb3f08db2e36" }) -- snippet completions
+  use({ "hrsh7th/cmp-nvim-lsp", commit = "affe808a5c56b71630f17aa7c38e15c59fd648a8" })
+  use({ "hrsh7th/cmp-nvim-lua", commit = "d276254e7198ab7d00f117e88e223b4bd8c02d21" })
+  use({ "glepnir/lspsaga.nvim", branch = 'main' })
 
-    -- use({ "hrsh7th/nvim-cmp" }) -- The completion plugin
-    -- use({ "hrsh7th/cmp-buffer" }) -- buffer completions
-    -- use({ "hrsh7th/cmp-path" }) -- path completions
-    -- use({ "saadparwaiz1/cmp_luasnip" }) -- snippet completions
-    -- use({ "hrsh7th/cmp-nvim-lsp" })
-    -- use({ "hrsh7th/cmp-nvim-lua" })
-    -- use({ "glepnir/lspsaga.nvim" })
 	-- snippets
-	use({ "L3MON4D3/LuaSnip", commit = "79b2019c68a2ff5ae4d732d50746c901dd45603a" }) --snippet engine
+
+	use({ "L3MON4D3/LuaSnip" }) --snippet engine
 	use({ "rafamadriz/friendly-snippets", commit = "d27a83a363e61009278b6598703a763ce9c8e617" }) -- a bunch of snippets to use
 
 
   --Lsp Stuff
-  use({ "neovim/nvim-lspconfig", commit = "148c99bd09b44cf3605151a06869f6b4d4c24455" }) -- enable LSP
-	use({ "williamboman/nvim-lsp-installer", commit = "e9f13d7acaa60aff91c58b923002228668c8c9e6" }) -- simple to use language server installer
+  use({ "williamboman/mason.nvim", branch = "fix/api-fast-event", "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig" })
 	use({ "jose-elias-alvarez/null-ls.nvim" }) -- for formatters and linters
 
   -- Telescope
@@ -148,9 +138,7 @@ return packer.startup(function(use)
   -- feline bar
   use { 'feline-nvim/feline.nvim', branch = '0.5-compat' }
 
-
-
-  -- wich key
+  -- which key
   use { "folke/which-key.nvim", commit = "bd4411a2ed4dd8bb69c125e339d837028a6eea71" }
 
   -- nvim-surround
