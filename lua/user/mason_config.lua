7f81dd5e969211d3b8818f2ff2fd95c2ceedf78b
Tomas <tomas.arregui@toremsoftware.com>
2023-01-29 11:31:29 -0300

    ADD: changed lsp-installer for mason

---
+++ lua/user/mason_config.lua
@@ -0,0 +1,60 @@
+local mason = SafeRequire("mason")
+local mason_config = SafeRequire("mason-lspconfig")
+
+
+local servers = {
+  "marksman",
+  "emmet_ls",
+  "jsonls",
+  "yamlls",
+  "sumneko_lua",
+  "tsserver",
+}
+
+mason.setup {
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+    }
+  }
+}
+
+mason_config.setup {
+  ensure_installed = servers,
+}
+
+require("lspconfig").sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        version = "Lua 5.3",
+        path = {
+          '?.lua',
+          '?/init.lua',
+          vim.fn.expand'~/.luarocks/share/lua/5.3/?.lua',
+          vim.fn.expand'~/.luarocks/share/lua/5.3/?/init.lua',
+          '/usr/share/5.3/?.lua',
+          '/usr/share/lua/5.3/?/init.lua'
+        }
+      },
+      workspace = {
+        library = {
+          vim.fn.expand'~/.luarocks/share/lua/5.3',
+          '/usr/share/lua/5.3'
+        }
+      }
+    }
+  }
+}
+
+local group = vim.api.nvim_create_augroup("MasonLspGroup", { clear = true })
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  group = group,
+  pattern = {"*.js", "*.ts"},
+  callback = function ()
+    require("lspconfig").tsserver.setup {}
+  end
+})
