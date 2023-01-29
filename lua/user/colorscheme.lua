c98b573b27e691465b54db12c3f171738b6dd8cf
Tomas <tomas.arregui@toremsoftware.com>
2023-01-29 11:35:00 -0300

    FIX: deleted gruvbox theme and improved onedark theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -15,7 +15,7 @@ onedark.setup {
     -- Options are italic, bold, underline, none
     -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
     code_style = {
-        comments = 'italic',
+        comments = 'none',
         keywords = 'none',
         functions = 'none',
         strings = 'none',
@@ -35,26 +35,3 @@ onedark.setup {
 }
 
 onedark.load()
-
--- local gruvbox = SafeRequire("gruvbox")
---
--- gruvbox.setup({
---   undercurl = true,
---   underline = true,
---   bold = true,
---   italic = true,
---   strikethrough = true,
---   invert_selection = false,
---   invert_signs = false,
---   invert_tabline = false,
---   invert_intend_guides = false,
---   inverse = true, -- invert background for search, diffs, statuslines and errors
---   contrast = "", -- can be "hard", "soft" or empty string
---   overrides = {},
---
--- })
---
--- vim.cmd("colorscheme gruvbox")
---
--- vim.o.background = "dark" -- or "light" for light mode
--- vim.cmd([[colorscheme gruvbox]])
