e51861956145386bb95b27a1391318784d04d894
Tomas <tomas.arregui@toremsoftware.com>
2023-01-29 18:15:37 -0300

    FIX: deleted problematic table. Now saga works OK

--- lua/user/lsp/saga.lua
+++ lua/user/lsp/saga.lua
@@ -85,35 +85,5 @@ saga.setup({
     -- auto refresh when change buffer
     auto_refresh = true,
   },
-
- ui = {
-    theme = 'round',
-    title = true,
-    border = 'solid',
-    winblend = 0,
-    expand = '',
-    collapse = '',
-    preview = ' ',
-    code_action = '💡',
-    diagnostic = '🐞',
-    incoming = ' ',
-    outgoing = ' ',
-    colors = {
-      --float window normal background color
-      normal_bg = '#1d1536',
-      --title background color
-      title_bg = '#afd700',
-      red = '#e95678',
-      magenta = '#b33076',
-      orange = '#FF8700',
-      yellow = '#f7bb3b',
-      green = '#afd700',
-      cyan = '#36d0e0',
-      blue = '#61afef',
-      purple = '#CBA6F7',
-      white = '#d1d4cf',
-      black = '#1c1c19',
-    },
-    kind = {},
-  },
 })
+
