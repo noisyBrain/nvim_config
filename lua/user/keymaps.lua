e1dc01d8d273ec264876a82ea8cd213baf005e6b
Tomas <tomas.arregui@toremsoftware.com>
2023-01-30 00:09:28 -0300

    REFACTOR: deleted comments

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,22 +1,10 @@
 local opts = { noremap = true, silent = true }
+local telescope_mapings = require('telescope.init')
 
--- local term_opts = { silent = true }
 
--- Shorten function name
 local keymap = vim.api.nvim_set_keymap
---Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
--- vim.g.maplocalleader = " "
-
--- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
-
 
 local mappings = {
 
