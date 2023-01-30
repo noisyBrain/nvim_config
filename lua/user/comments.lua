140eece0abe6c1d7a198812ac29a98cd96bebfb1
Tomas <tomas.arregui@toremsoftware.com>
2023-01-30 00:07:18 -0300

    FIX: line comments instead block comments in lua

--- lua/user/comments.lua
+++ lua/user/comments.lua
@@ -1,67 +1,39 @@
 local comment = SafeRequire("Comment")
 comment.setup {
-
-    -- ---Add a space b/w comment and the line
     padding = true,
-
-    -- ---Whether the cursor should stay at its position
     sticky = true,
-
-    -- ---Lines to be ignored while (un)comment
     ignore = nil,
 
-    -- ---LHS of toggle mappings in NORMAL mode
-    toggler = {
-        ---Line-comment toggle keymap
-        line = 'gcc',
-        ---Block-comment toggle keymap
-        block = 'gbc',
-    },
+    toggler = { line = "gcc", block = "<Nop>" },
+    opleader = { line = "gc", block = "<Nop>" },
+    mappings = { basic = true, extra = true },
 
-    -- ---LHS of operator-pending mappings in NORMAL and VISUAL mode
-    opleader = {
-        ---Line-comment keymap
-        line = 'gc',
-        ---Block-comment keymap
-        block = 'gb',
-    },
+    extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
 
-    -- ---LHS of extra mappings
-    extra = {
-        ---Add comment on the line above
-        above = 'gcO',
-        ---Add comment on the line below
-        below = 'gco',
-        ---Add comment at the end of line
-        eol = 'gcA',
-    },
-
-    -- ---Enable keybindings
-    -- ---NOTE: If given `false` then the plugin won't create any mappings
-    mappings = {
-        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
-        basic = true,
-        ---Extra mapping; `gco`, `gcO`, `gcA`
-        extra = true,
-    },
-
-    -- ---Function to call before (un)comment
     pre_hook = function(ctx)
-      local U = require "Comment.utils"
+      local filetype = vim.api.nvim_get_option_value("filetype", { buf = 0 })
 
-      local location = nil
-      if ctx.ctype == U.ctype.block then
-        location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-        location = require("ts_context_commentstring.utils").get_visual_start_location()
+      if filetype == "javascriptreact" or filetype == "typescriptreact" then
+        local ctx_ctype = ctx.ctype
+        local type = ctx_ctype == require("Comment.utils").ctype.linewise and "__default" or "__multiline"
+        local location = nil
+        local ctx_cmotion = ctx.cmotion
+
+        if ctx_ctype == require("Comment.utils").ctype.blockwise then
+          location = require("ts_context_commentstring.utils").get_cursor_location()
+        elseif
+          ctx_cmotion == require("Comment.utils").cmotion.v or ctx_cmotion == require("Comment.utils").cmotion.V
+        then
+          location = require("ts_context_commentstring.utils").get_visual_start_location()
+        end
+
+        return require("ts_context_commentstring.internal").calculate_commentstring({
+          key = type,
+          location = location,
+        })
       end
+    end,
 
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-        key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-        location = location,
-      } end,
-
-    -- ---Function to call after (un)comment
     post_hook = nil,
-} -- end,
+}
 
