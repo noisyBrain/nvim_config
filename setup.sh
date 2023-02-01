8abb44336d888586c023e6412dfd54bf7be9e02a
Tomas <tomas.arregui@toremsoftware.com>
2023-02-01 19:16:16 -0300

    FIX: correct repository name for installer

--- setup.sh
+++ setup.sh
@@ -6,13 +6,12 @@ rm -rf $HOME/.config/nvim
 rm -rf $HOME/.local/share/nvim
 rm -rf $HOME/.cache/nvim
 
-echo "Clean comple, now install packer"
+echo "Clean completed, now install packer"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 clear
 echo "Packer installed sucefully 😃😃😃"
 
 echo "now install neovim config"
-git clone https://github.com/noisyBrain/updated_nvim_config.git $HOME/.config/nvim
-
+git clone https://github.com/noisyBrain/nvim_config.git $HOME/.config/nvim
 
 echo "Neovim is now installed"
