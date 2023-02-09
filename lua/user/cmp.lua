e2556417569ed9f7692d14e69f1525114d913a40
Tomas <tomas.arregui@toremsoftware.com>
2023-02-08 23:54:52 -0300

    ADD: cmp for vim api

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -95,6 +95,7 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
+        nvim_lua = "[NVIM ]",
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
@@ -103,6 +104,7 @@ cmp.setup {
     end,
   },
   sources = {
+    { name = "nvim_lua" },
     { name = "nvim_lsp" },
     { name = "luasnip" },
     { name = "buffer" },
