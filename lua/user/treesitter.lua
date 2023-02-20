88f6fad7b7f28aca4c1e7a9d95dcff62ddfdd14b
Tomás <tomasarreguib@gmail.com>
2023-02-19 23:12:55 -0300

    CHANGE: improvements for nvim options and ts rainbow

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -14,9 +14,10 @@ configs.setup({
   rainbow = {
     enable = true,
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    query = 'rainbow-parens',
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
   }
 })
+
+
