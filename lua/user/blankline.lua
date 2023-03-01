56fd641c2af9113fb6322e27668033ae6c773db7
Tomás <tomasarreguib@gmail.com>
2023-03-01 14:50:24 -0300

    CHANGE: explanation of setup in blankline, default color for blankline_indent

--- lua/user/blankline.lua
+++ lua/user/blankline.lua
@@ -1,12 +1,21 @@
 local blankline = SafeRequire("indent_blankline")
 
-vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#00cc00 gui=nocombine]]
 
+-- setup different colours for blankline example:
+
+--vim.opt.termguicolors = true -> Enables 24-bit RGB color in the |TUI|.  Uses "gui" |:highlight|
+                               -- 	attributes instead of "cterm" attributes. |guifg|
+                               -- 	Requires an ISO-8613-3 compatible terminal.
+
+-- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#00cc00 gui=nocombine]] -> setup gui foreground to display indent line
+
+-- blankline.setup {
+  -- char_highlight_list = {
+  --   "IndentBlanklineIndent1",
+  -- },
+  -- show_trailing_blankline_indent = false
+-- }
 
 blankline.setup {
-  char_highlight_list = {
-    "IndentBlanklineIndent1",
-  },
   show_trailing_blankline_indent = false
 }
