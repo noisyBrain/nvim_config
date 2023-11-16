83dbc5210a381f94cd79c43b2aba5cb1eace377c
Tomás <tomasarreguib@gmail.com>
2023-11-16 10:14:13 -0300

    chore(onedark): changed font type for theme

--- lua/user/onedark.lua
+++ lua/user/onedark.lua
@@ -16,7 +16,7 @@ onedark.setup {
     -- You can configure multiple style with comma seperated, For e.g., keywords = 'italic,bold'
     code_style = {
         comments = 'none',
-        keywords = 'none',
+        keywords = 'italic',
         functions = 'bold',
         strings = 'none',
         variables = 'none'
