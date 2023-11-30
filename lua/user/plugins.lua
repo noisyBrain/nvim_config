ba3650b838476bac267cb4de48380d332f336121
Tomás <tomasarreguib@gmail.com>
2023-11-29 21:09:19 -0300

    fix(treesitter): changed ts-rainbow for rainbow-delimiters

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -90,6 +90,7 @@ return packer.startup(function(use)
 
   -- Treesitter
   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+  use { "HiPhish/rainbow-delimiters.nvim" }
 
   -- UI
   use { "stevearc/dressing.nvim" }
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = SafeRequire("nvim-treesitter.configs")
 
 configs.setup({
-	ensure_installed = { 'typescript', 'javascript', 'html', 'css' }, -- one of "all" or a list of languages
+	ensure_installed = { 'typescript', 'javascript', 'html', 'css', 'python', 'prisma', 'cpp' }, -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
@@ -13,9 +13,8 @@ configs.setup({
 	indent = { enable = true, disable = { "" } },
   rainbow = {
     enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    query = 'rainbow-parens',
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    query = "rainbow-delimiters",
+    extended_mode = true,
+    max_file_lines = nil,
   }
 })
